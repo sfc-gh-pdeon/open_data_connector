82a4020b3f5a06d82def7a8f402f52abcfd041c9
Paul D'Eon <163459166+sfc-gh-pdeon@users.noreply.github.com>
2024-09-10 17:44:41 -0400

    Update 4_status.py

--- streamlit/pages/4_status.py
+++ streamlit/pages/4_status.py
@@ -33,7 +33,7 @@ with col1:
             session.sql(f"
              BEGIN 
                show tasks in {app_name}.core;
-               LET c1 cursor for (SELECT '{app_name}.core.' || "name" as TNAME FROM TABLE(RESULT_SCAN(LAST_QUERY_ID())));
+               LET c1 cursor for (SELECT '{app_name}.core.' || "name" as tname FROM TABLE(RESULT_SCAN(LAST_QUERY_ID())));
                open c1;
                FOR arow in c1 DO
                   let taskname varchar := arow.tname;
@@ -52,7 +52,7 @@ with col2:
             session.sql(f"
              BEGIN 
                show tasks in {app_name}.core;
-               LET c1 cursor for (SELECT '{app_name}.core.' || "name" as TNAME FROM TABLE(RESULT_SCAN(LAST_QUERY_ID())));
+               LET c1 cursor for (SELECT '{app_name}.core.' || "name" as tname FROM TABLE(RESULT_SCAN(LAST_QUERY_ID())));
                open c1;
                FOR arow in c1 DO
                   let taskname varchar := arow.tname;
